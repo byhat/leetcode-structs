66d8907558453a3c44698cf0a8d4b715275beefd
bhat <encomblackhat@gmail.com>
2023-07-14 16:23:28 -0700

    Add Memo2D.

--- src/dp/memo.rs
+++ src/dp/memo.rs
@@ -40,3 +40,49 @@ impl<T: Clone> Memo<usize, T> for Memo1D<T> {
         ret
     }
 }
+
+pub struct Memo2D<T> {
+    pending: Vec<Vec<bool>>,
+    inner: Vec<Vec<Option<T>>>,
+    m: usize,
+    n: usize,
+}
+
+impl<T: Clone> Memo2D<T> {
+    pub fn new(m: usize, n: usize) -> Self {
+        let pending = vec![vec![false; n + 1]; m + 1];
+        let inner = vec![vec![None; n + 1]; m + 1];
+        Self {
+            pending,
+            inner,
+            m,
+            n,
+        }
+    }
+}
+
+impl<T: Clone> Memo<(usize, usize), T> for Memo2D<T> {
+    fn exec(
+        &mut self,
+        p: &impl Solver<(usize, usize), T>,
+        (token_0, token_1): (usize, usize),
+    ) -> Option<T> {
+        if token_0 > self.m || token_1 > self.n {
+            return None;
+        }
+        if self.pending[token_0][token_1] {
+            return None;
+        }
+        if let Some(ret) = self.inner[token_0][token_1].clone() {
+            return Some(ret);
+        }
+
+        self.pending[token_0][token_1] = true;
+        let ret = p.exec(self, (token_0, token_1));
+        self.pending[token_0][token_1] = false;
+
+        self.inner[token_0][token_1] = ret.clone();
+
+        ret
+    }
+}
