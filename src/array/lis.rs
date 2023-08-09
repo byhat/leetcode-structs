d99c37afddb7bf5819e0e9b04c6b7b790cac7b86
bhat <encomblackhat@gmail.com>
2023-08-08 19:33:56 -0700

    Add LNdS.

--- src/array/lis.rs
+++ src/array/lis.rs
@@ -1,4 +1,4 @@
-pub fn longest_increasing_subsequence(vec: Vec<i32>) -> usize {
+pub fn longest_inc_subseq(vec: Vec<i32>) -> usize {
     let mut stack = vec![]; // monotonic stack
     for e in vec {
         let e_max = if let Some(inner) = stack.last() {
@@ -14,6 +14,28 @@ pub fn longest_increasing_subsequence(vec: Vec<i32>) -> usize {
         }
 
         let ptr = stack.partition_point(|&_e| _e < e);
+        stack[ptr] = e;
+    }
+
+    stack.len()
+}
+
+pub fn longest_nondec_subseq(vec: Vec<i32>) -> usize {
+    let mut stack = vec![]; // monotonic stack
+    for e in vec {
+        let e_max = if let Some(inner) = stack.last() {
+            *inner
+        } else {
+            stack.push(e);
+            continue;
+        };
+
+        if e >= e_max {
+            stack.push(e);
+            continue;
+        }
+
+        let ptr = stack.partition_point(|&_e| _e <= e);
         stack[ptr] = e;
     }
 
