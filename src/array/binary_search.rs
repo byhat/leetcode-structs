195d7ed52b3fc4b4b25387b3369693358929585c
bhat <encomblackhat@gmail.com>
2023-08-16 00:23:00 -0700

    Add floating point version of partition_point().

--- src/array/binary_search.rs
+++ src/array/binary_search.rs
@@ -20,3 +20,20 @@ pub fn partition_point<P: FnMut(usize) -> bool>(p0: usize, p1: usize, mut predic
 
     partition_point(p0_next, p1_next, predicate) // tail recursion
 }
+
+pub fn partition_point_fp<P: FnMut(f64) -> bool>(p0: f64, p1: f64, mut predicate: P) -> f64 {
+    const EP: f64 = 0.0000001;
+
+    if p0 + EP > p1 {
+        return p0;
+    } // be careful about e.p.!!
+
+    let p_decision = p0 + (p1 - p0) / 2f64;
+    let (p0_next, p1_next) = if predicate(p_decision) {
+        (p_decision + EP, p1)
+    } else {
+        (p0, p_decision)
+    };
+
+    partition_point_fp(p0_next, p1_next, predicate) // tail recursion
+}
